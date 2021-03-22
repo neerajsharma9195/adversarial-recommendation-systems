29ccb2bcbeb01c50a61935b411ed6b5769617123
Neeraj Sharma <neerajsharma9195@gmail.com>
2021-03-22 11:25:37 -0400

    Fix: fixed devide issue

--- src/models/arcf.py
+++ src/models/arcf.py
@@ -39,7 +39,7 @@ def train(rating_generator, missing_generator, rating_discriminator,
             for i, batch in enumerate(train_dataloader):
                 review_embedding, rating_vector, conditional_vector = batch
                 rating_vector = rating_vector.to(device)
-                conditional_vector = conditional_vector
+                conditional_vector = conditional_vector.to(device)
                 review_embedding = review_embedding.squeeze(0).to(device)
                 real_missing_vector = torch.tensor((rating_vector>0)*1).to(device)
                 noise_vector = torch.tensor(np.random.normal(0, 1, noise_size).reshape(1, noise_size), dtype=torch.float32).to(device)
