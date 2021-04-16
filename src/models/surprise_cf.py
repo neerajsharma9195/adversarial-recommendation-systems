2e5d3457240d6860ef26d5df1aa68a23226b5861
Liza Bialik <liza.bialik@gmail.com>
2021-04-16 11:49:50 -0400

    verbose=False for algos

--- src/models/surprise_cf.py
+++ src/models/surprise_cf.py
@@ -12,20 +12,24 @@ class Model():
         self.ks = ks
 
     def train(self, trainset):
-        print('training ', self.name, '...', end='')
+        print('training ', self.name, '... ', end='')
         start = time.time()
         self.algo.fit(trainset)
         end = time.time()
-        print('done in ', round(end-start), ' seconds')
-
+        print('done training ', self.name, ' in ', round(end-start), ' seconds')
+    
     def predict(self, testset):
         self.predictions = self.algo.test(testset)
 
     def evaluate(self):
-        self.mae = accuracy.mae(self.predictions)
-        self.rmse = accuracy.rmse(self.predictions)
+        print('evaluating ', self.name, '... ', end='')
+        start = time.time()
+        self.mae = accuracy.mae(self.predictions, verbose=False)
+        self.rmse = accuracy.rmse(self.predictions, verbose=False)
         precisions_and_recalls = [precision_recall_at_k(self.predictions, k) for k in self.ks]
         self.MAPs, self.MARs = zip(*precisions_and_recalls)
+        end = time.time()
+        print('done evaluating ', self.name, ' in ', round(end-start), ' seconds')
 
 def run_model(model, trainset, testset):
     model.train(trainset)
@@ -38,9 +42,9 @@ def run(masked_R_coo, unmasked_vals_coo, mask_coo, mask_csr, ks):
     trainset, testset = setup(masked_R_coo, unmasked_vals_coo)
     models = [
         Model(name='random', algo=NormalPredictor(), ks=ks),
-        Model(name='SGD', algo=BaselineOnly(bsl_options = {'method': 'sgd','learning_rate': .00005,}), ks=ks),
-        Model(name='SVD', algo=SVD(), ks=ks),
-        # Model(name='KNN', algo=KNNBasic())
+        Model(name='SGD', algo=BaselineOnly(verbose=False, bsl_options = {'method': 'sgd','learning_rate': .00005,}), ks=ks),
+        # Model(name='KNN', algo=KNNBasic(verbose=False), ks=ks),
+        Model(name='SVD', algo=SVD(verbose=False), ks=ks),
         ]
 
     args = [(model, trainset, testset) for model in models]
